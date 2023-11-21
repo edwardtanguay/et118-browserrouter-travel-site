96dd3e76186a976b3af671d9e65328ce8205a973
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-21 04:17:14 +0100

    new link style

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,7 @@
 export const PageWelcome = () => {
 	return (
-		<p>this is the welcome page</p>
+		<p className="p-1 leading-8">
+			{/* Welcome to our travel agency! Get to know our team, find your next travel destination and plan your trip! */}
+		</p>
 	)
 }
