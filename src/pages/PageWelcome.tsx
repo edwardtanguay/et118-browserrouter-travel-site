0eb7a88b83f5dd8efc999db0be19da903460ab76
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-21 04:26:08 +0100

    welcome links

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,9 @@
+import { NavLink } from "react-router-dom"
+
 export const PageWelcome = () => {
 	return (
 		<p className="p-1 leading-8">
-			{/* Welcome to our travel agency! Get to know our team, find your next travel destination and plan your trip! */}
+			Welcome to our travel agency! Get to know our <NavLink to="/team">team</NavLink>, find your next travel <NavLink to="/destinations">destination</NavLink> and <NavLink to="/planner">plan</NavLink> your trip!
 		</p>
 	)
 }
